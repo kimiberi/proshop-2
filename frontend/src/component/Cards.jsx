3c7458260d42806f0c56ee67ffef773e59cec3d1
kimiberi <kim.villarmente@gmail.com>
2025-02-24 11:48:36 +0800

    add asc / desc

--- frontend/src/component/Cards.jsx
+++ frontend/src/component/Cards.jsx
@@ -8,6 +8,10 @@ import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid2";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 // import axios from "axios";
 import _ from "lodash";
 import moment from "moment";
@@ -24,22 +28,10 @@ const Cards = () => {
 
   const [defaultPost, setDefaultPost] = useState([]);
   const [search, setSearch] = useState("");
-
-  const onSearchName = (name) => {
-    setSearch(name);
-    console.log(search);
-    console.log(defaultPost);
-
-    const searchTerms = _.split(_.toLower(search), " ");
-
-    const results = _.filter(defaultPost, (item) =>
-      _.some(searchTerms, (term) => _.includes(_.toLower(item.name), term))
-    );
-
-    console.log(results);
-    dispatch(list(results));
-    console.log(isTypeListData);
-  };
+  const [opt, setOpt] = useState("asc");
+  const [post, setPost] = useState(true);
+  const [post1, setPost1] = useState([]);
+  const [sortedData, setSortedData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -50,33 +42,121 @@ const Cards = () => {
     fetchData();
   }, []); // Runs once when component mounts
 
-  // useEffect(() => {
-  //   // Partial search
+  const onSearchName = (name) => {
+    setSearch(name);
+    // console.log(search);
+    // console.log(defaultPost);
 
-  // }, []);
+    const searchWords = _.split(_.toLower(search), " ");
+
+    const results = _.filter(defaultPost, (item) =>
+      _.some(searchWords, (word) => _.includes(_.toLower(item.name), word))
+    );
+
+    // console.log(results);
+    dispatch(list(results));
+    // console.log(isTypeListData);
+  };
+
+  const onAscDesc = (option) => {
+    console.log(option);
+    setOpt(option);
+
+    // Sorting function
+    // const sortData = (data, order) => {
+    //   return _.orderBy(data, ["id"], [order]); // "asc" for ascending, "desc" for descending
+    // };
+
+    // console.log(option);
+    // if (option === "asc") {
+    //   // Ascending order
+    //   console.log(sortData(isTypeListData, "asc"));
+    //   // Output: [{ id: 1, name: "Morty" }, { id: 2, name: "Summer" }, { id: 3, name: "Rick" }]
+    // }
+
+    // if (option === "desc") {
+    //   // Descending order
+    //   console.log(sortData(isTypeListData, "desc"));
+    //   const result = sortData(isTypeListData, "desc");
+    //   // Output: [{ id: 3, name: "Rick" }, { id: 2, name: "Summer" }, { id: 1, name: "Morty" }]
+    //   dispatch(list(result));
+    // }
+  };
+
+  useEffect(() => {
+    console.log("Sorting with option:", opt);
+
+    const sortData = (data, order) => {
+      return _.orderBy(data, ["id"], [order]); // Sort by "id"
+    };
+
+    const result =
+      opt === "desc"
+        ? sortData(isTypeListData, "desc")
+        : sortData(isTypeListData, "asc");
+
+    console.log("Sorted Result:", result);
+
+    // Check if new sorted data is different before updating state
+    if (!_.isEqual(sortedData, result)) {
+      setSortedData(result);
+    }
+  }, [opt, isTypeListData]); // Dependencies
+
+  // Dispatch to Redux when `sortedData` changes
+  useEffect(() => {
+    console.log(sortedData);
+
+    if (sortedData.length > 0) {
+      // not working
+      dispatch(list(sortedData));
+      console.log(isTypeListData);
+    }
+  }, [sortedData, dispatch]); // Dependencies
 
   return (
     <div style={{ width: "80%", background: "#f8ebe0", padding: "50px" }}>
       <Stack direction="row" spacing={2}>
-        <Button variant="contained" onClick={(e) => onSearchName(search)}>
-          Search
-        </Button>
-        <TextField
-          id="filled-basic"
-          label="Search name"
-          variant="filled"
-          sx={{ width: "40%" }}
-          onChange={(e) => {
-            setSearch(e.target.value);
-            console.log(e.target.value);
-          }}
-        />
+        <div style={{ width: "50%" }}>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" onClick={(e) => onSearchName(search)}>
+              Search
+            </Button>
+            <TextField
+              id="filled-basic"
+              label="Search name"
+              variant="filled"
+              sx={{ width: "100%" }}
+              onChange={(e) => {
+                setSearch(e.target.value);
+                // console.log(e.target.value);
+              }}
+            />
+          </Stack>
+        </div>
+
+        <div style={{ width: "50%" }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Option</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              defaultValue={opt}
+              value={opt}
+              label="Option"
+              onChange={(e) => onAscDesc(e.target.value)}
+            >
+              <MenuItem value="asc">Ascending</MenuItem>
+              <MenuItem value="desc">Descending</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
       </Stack>
       <br />
       <br />
 
       <Grid container spacing={5}>
-        {_.map(isTypeListData, (data) => (
+        {_.map(sortedData ? sortedData : isTypeListData, (data) => (
           <Grid size={4} key={data.id}>
             <Card sx={{ maxWidth: 345 }}>
               <CardMedia
